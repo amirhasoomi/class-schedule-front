7d9733a119dfe74b18d82d3434ec0235c0211543
Amirhossein Hasoomi <a.h.hasoomi@gmail.com>
2021-06-17 09:47:40 +0430

    banner ans slide

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,20 @@
 import axios from "axios";
 import { BASE_URL } from "../config";
 
+// request interceptor
+axios.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem('acc');
+        if (token) {
+            // config.headers["Authorization"] = "Bearer " + token;
+            config.headers["Authorization"] = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => {
+        Promise.reject(error);
+    }
+);
 
 // login:
 
@@ -8,7 +22,7 @@ export const LoginAxios = async ({ mobile, password }) => {
     return await axios.post(`${BASE_URL}auth/login`, { mobile, password }).then((result) => {
         localStorage.setItem('ref', result.data.refresh);
         localStorage.setItem('acc', result.data.access);
-        localStorage.setItem('user-type', result.data.user_type);
+        localStorage.setItem('user_type', result.data.user_type);
     });
 };
 
@@ -22,6 +36,35 @@ export const getSliderAxios = async () => {
     });
 };
 
+// delete sliders:
+// http://127.0.0.1:8000/api/features/slider/id
+export const deleteSlideAxios = async (slideId) => {
+    return await axios({
+        method: "delete",
+        url: `${BASE_URL}features/slider/${slideId}`,
+    });
+};
+
+// put sliders:
+// "http://127.0.0.1:8000/api/features/slider/id
+export const putSlideAxios = async (slideId, { data }) => {
+    return await axios({
+        method: "put",
+        url: `${BASE_URL}features/slider/${slideId}`,
+        data,
+    });
+};
+
+// post sliders:
+// http://127.0.0.1:8000/api/features/slider/
+export const postSlideAxios = async (data) => {
+    return await axios({
+        method: "post",
+        url: `${BASE_URL}features/slider`,
+        data,
+    });
+};
+
 
 // get banners:
 // http://127.0.0.1:8000/api/features/banner
@@ -30,6 +73,26 @@ export const getBannerAxios = async () => {
     return await axios({
         method: "get",
         url: `${BASE_URL}features/banner`,
+    });
+};
+
+// delete banners:
+// http://127.0.0.1:8000/api/features/banner
+
+export const deleteBannerAxios = async (id) => {
+    return await axios({
+        method: "delete",
+        url: `${BASE_URL}features/banner/${id}`,
+    });
+};
+
+// post banners:
+// http://127.0.0.1:8000/api/features/banner/
+export const postBannerAxios = async (data) => {
+    return await axios({
+        method: "post",
+        url: `${BASE_URL}features/banner`,
+        data,
     });
 };
 
@@ -56,10 +119,11 @@ export const getSupportsAxios = async () => {
 // Post message:
 // http://127.0.0.1:8000/api/features/contact
 
-export const Postmessage = async (data) => {
+export const PostmessageAxios = async (data) => {
     return await axios({
-      method: "post",
-      url: `${BASE_URL}features/contact`,
-      data,
+        method: "post",
+        url: `${BASE_URL}features/contact`,
+        data,
     });
-  };
+};
+
